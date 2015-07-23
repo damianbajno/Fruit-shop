03a8a06c7ed564a2235737f176dabd1582d891cc
damianbajno <damian.bajno@gmail.com>
2015-07-23 03:07:31 +0200

    CommiteAll

---
+++ Fruite-shop/Fruite-Shop/src/pl/damian/xml/mainXML.java
@@ -0,0 +1,32 @@
+package pl.damian.xml;
+
+import java.beans.ExceptionListener;
+import java.beans.XMLDecoder;
+import java.beans.XMLEncoder;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import pl.damian.main.Fruite;
+
+public class mainXML {
+
+	
+
+	public static void main(String args[]) {
+
+		Fruite[] fruites = {
+				new Fruite("pl", "Jabłko", 3,
+						"/home/damian/git/Fruite-Shop/Fruite-Shop/resources/apple.jpg"),
+				new Fruite("pl", "Gruszka", 2,
+						"/home/damian/git/Fruite-Shop/Fruite-Shop/resources/pear.jpg") };
+		FruitXML fruitXML=new FruitXML();
+		fruitXML.writeFruitesToXMLFile(fruites);
+		
+	}
+
+	
+
+}
